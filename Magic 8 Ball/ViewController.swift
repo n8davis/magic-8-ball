ca9ac9e5d61cd33962bab620570e98e686bdbde0
n8davis@me.com <n8davis@me.com>
2019-03-25 19:16:06 -0500

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Nate Davis on 3/25/19.
+//  Copyright © 2019 Nate Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
